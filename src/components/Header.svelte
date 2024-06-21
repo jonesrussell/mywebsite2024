e97252fbac8719f52a9fc2f428a8c3c8b2bd61f1
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-06-21 13:16:19 +0000

    Update titile bar text

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,13 +1,7 @@
 <header class="flex flex-col">
 	<div class="bg-[#cfe0fc] w-full border-b border-black flex justify-around">
-		<span>Amiga Workbench Release 3.2</span>
-		<span>2.015.616</span>
-		<span>Chip-RAM</span>
-		<span>395.810.080</span>
-		<span>sonstiges RAM</span>
-		<span>CPU 68040</span>
-		<span>11 active Applications</span>
-		<span>Saturday 24-Oct-20</span>
-		<span>11:20:33</span>
+		<span>Amiga Workbench</span>
+		<span>1,912,400 graphics mem</span>
+		<span>7,686,560 other mem</span>
 	</div>
 </header>
