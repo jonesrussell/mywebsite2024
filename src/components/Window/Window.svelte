6241164b057ef0414c4facde3e129a63d1e881c7
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-06-27 05:58:41 +0000

    Dragable and resizable

--- src/components/Window/Window.svelte
+++ src/components/Window/Window.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Moveable from "svelte-moveable";
+	import Moveable from 'svelte-moveable';
 	import WindowHeader from './WindowHeader.svelte';
 	import WindowFooter from './WindowFooter.svelte';
 
@@ -7,18 +7,29 @@
 
 	let winHeader: HTMLElement;
 	let targetRef: HTMLElement | null = null;
+	let moveableRef = null;
 
 	const draggable = true;
 	const throttleDrag = 1;
 	const edgeDraggable = false;
 	const startDragRotate = 0;
 	const throttleDragRotate = 0;
+
+	const maxWidth = 'auto';
+	const maxHeight = 'auto';
+	const minWidth = 'auto';
+	const minHeight = 'auto';
+	const resizable = true;
+	const keepRatio = false;
+	const throttleResize = 1;
+	const renderDirections = ['nw', 'n', 'ne', 'w', 'e', 'sw', 's', 'se'];
 </script>
 
 <section
 	aria-label={title}
-	class="absolute transform border border-gray-500 bg-gray-200 shadow-lg md:w-1/2"
+	class="target absolute transform border border-gray-500 bg-gray-200 shadow-lg md:w-1/2"
 	bind:this={targetRef}
+	style={`max-width: ${maxWidth};max-height: ${maxHeight};min-width: ${minWidth};min-height: ${minHeight};`}
 >
 	<div bind:this={winHeader}>
 		<WindowHeader {title} />
@@ -29,18 +40,26 @@
 	</div>
 
 	<WindowFooter />
-
-	<Moveable
-		target={targetRef}
-		draggable={draggable}
-		throttleDrag={throttleDrag}
-		edgeDraggable={edgeDraggable}
-		startDragRotate={startDragRotate}
-		throttleDragRotate={throttleDragRotate}
-		on:drag={({ detail: e }) => {
-			if (targetRef) {
-				targetRef.style.transform = e.transform;
-			}
-		}}
-	/>
 </section>
+
+<Moveable
+	bind:this={moveableRef}
+	target={targetRef}
+	{draggable}
+	{throttleDrag}
+	{edgeDraggable}
+	{startDragRotate}
+	{throttleDragRotate}
+	{resizable}
+	{keepRatio}
+	{throttleResize}
+	{renderDirections}
+	on:drag={({ detail: e }) => {
+		e.target.style.transform = e.transform;
+	}}
+	on:resize={({ detail: e }) => {
+		e.target.style.width = `${e.width}px`;
+		e.target.style.height = `${e.height}px`;
+		e.target.style.transform = e.drag.transform;
+	}}
+/>
