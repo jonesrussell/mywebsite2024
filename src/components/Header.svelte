02ffcabb665fdd8bf6b7086a2b7b4420ecc3bdf8
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-06-27 07:28:40 +0000

    Repsonsive header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,7 @@
 <header class="flex flex-col">
 	<div class="bg-[#cfe0fc] w-full border-b border-black flex justify-around">
 		<span>Amiga Workbench</span>
-		<span>1,912,400 graphics mem</span>
-		<span>7,686,560 other mem</span>
+		<span class="hidden md:block">1,912,400 graphics mem</span>
+		<span class="hidden md:block">7,686,560 other mem</span>
 	</div>
 </header>
