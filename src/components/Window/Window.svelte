8c9a02eaff82bf559f77e0cf19af93c3d59ef5f0
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-06-27 04:22:44 +0000

    draggable

--- src/components/Window/Window.svelte
+++ src/components/Window/Window.svelte
@@ -1,36 +1,17 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { draggable } from '@neodrag/svelte';
 	import WindowHeader from './WindowHeader.svelte';
 	import WindowFooter from './WindowFooter.svelte';
-	import { calculateInitialPosition, makeElementDraggable } from './windowUtils';
-
-	let x = 0;
-	let y = 0;
-	let winHeader: HTMLElement;
 
 	export let title: string = '';
-
-	onMount(() => {
-		const interval = setInterval(() => {
-			if (winHeader) {
-				calculateInitialPosition(winHeader, (newX, newY) => {
-					x = newX;
-					y = newY;
-				});
-				clearInterval(interval);
-			}
-		}, 100);
-	});
 </script>
 
 <section
 	aria-label={title}
 	class="absolute transform border border-gray-500 bg-gray-200 shadow-lg md:w-1/2"
-	style="transform: translate3d({x}px, {y}px, 0);"
+	use:draggable
 >
-	<div bind:this={winHeader} use:makeElementDraggable={{ x, y, header: winHeader }}>
-		<WindowHeader {title} />
-	</div>
+	<WindowHeader {title} />
 
 	<div class="border-l-[16px] border-r-[16px] border-gray-500 p-4">
 		<slot />
